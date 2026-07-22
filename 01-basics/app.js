70592bc84390c9bfa68bcf4af82e51f709d5ba99
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-07-22 22:35:35 +0530

    Add files via upload

--- 01-basics/app.js
+++ 01-basics/app.js
@@ -1,64 +1,100 @@
 const express = require("express");
-
 const app = express();
 
+// Middleware
 app.use(express.json());
 
 let books = [
   {
-    id: 1,
-    title: "Book 1",
+    id: "1",
+    title: "book 10",
   },
   {
-    id: 2,
-    title: "Book 2",
+    id: "2",
+    title: "book 2",
   },
   {
-    id: 3,
-    title: "Book 3",
+    id: "3",
+    title: "book 3",
   },
 ];
 
 // Intro route
 app.get("/", (req, res) => {
   res.json({
-    message: "Welcome to our bookstore API",
+    message: "Welcome to our Bookstore API",
   });
 });
 
 // Get all books
-app.get("/books", (req, res) => {
+app.get("/get", (req, res) => {
   res.json(books);
 });
 
 // Get a single book
-app.get("/books/:id", (req, res) => {
-  const book = books.find((item) => item.id === Number(req.params.id));
+app.get("/get/:id", (req, res) => {
+  const book = books.find((item) => item.id === req.params.id);
 
   if (book) {
     res.status(200).json(book);
   } else {
     res.status(404).json({
-      message: "Book not found! Please try with a different ID.",
+      message: "Book not found! Please try with different Book ID",
     });
   }
 });
 
-// add a new book
-
+// Add a new book
 app.post("/add", (req, res) => {
   const newBook = {
-    id: books.length + 1,
-    title: `Book ${books.length + 1}`,
+    id: Math.floor(Math.random() * 1000).toString(),
+    title: `Book ${Math.floor(Math.random() * 1000)}`,
   };
 
   books.push(newBook);
+
   res.status(200).json({
-  data: newBook,
-  message: "New Book is added successfully"
-  
-  
-  })
+    data: newBook,
+    message: "New Book is added successfully",
+  });
+});
+
+// update a book
+app.put("/update/:id", (req, res) => {
+  const findCurrentBook = books.find(
+    (bookItem) => bookItem.id === req.params.id,
+  );
+  if (findCurrentBook) {
+    findCurrentBook.title = req.body.title || findCurrentBook.title;
+
+    res.status(200).json({
+      message: `Book with ID ${req.params.id} updated successfully`,
+      data: findCurrentBook,
+    });
+  } else {
+    res.status(404).json({
+      message: "Book not found ",
+    });
+  }
+});
+
+// delete the book
+
+app.delete("/delete/: id", (req, res) => {
+  const findIndexOfCurrentBook = books.findIndex(
+    (item) => item.id === req.params.id,
+  );
+  if (findIndexOfCurrentBook !== -1) {
+    const deletedBook = books.splice(findIndexOfCurrentBook, 1);
+    res.status(200).json({
+      message: "Book deleted successfully",
+      data: deleted[0],
+    });
+  } else {
+    res.status(404).json({
+      message: "Book not found",
+    });
+  }
 });
 
 const port = 3000;
